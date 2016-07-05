9e241b36e188c90e50072e1ab9f6ab662a60112d
Cody Scholberg <cscholberg@gmail.com>
2016-07-05 09:28:41 -0400

    tryin'a get it workin'

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ var http = require('http');
 var morgan = require('morgan');             
 var bodyParser = require('body-parser');    
 var methodOverride = require('method-override');
-
-app.use(express.static(__dirname + './client'));                
+var path = require('path');
+              
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json()); 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+app.use('/client', express.static(path.join(__dirname, './client')));
+
 app.get('*', function(req, res) {
-  res.sendfile('./index.html');
+  res.sendfile('./client/index.html');
 });
 
 var port = process.env.PORT || 3000;
 
-app.listen();
+app.listen(port);
 console.log("App listening on port 3000");
 
 exports = module.exports = app;
